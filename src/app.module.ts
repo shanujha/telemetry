388c5398ee5e477bc30d59926f3653befd43224a
Shanu Jha <shanu.j@foxberry.in>
2024-09-11 23:50:24 +0530

    add modules and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TelemetryModule } from './telemetry/telemetry.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TelemetryModule],
+  imports: [TelemetryModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'dev_user',
+    password: 'dev_password',
+    database: 'development_db',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true, // Note: Disable this in production
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
